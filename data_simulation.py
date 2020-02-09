bf14f66d67f90345728dae04ab79eaca20863f7e
cherylin <cherylin@umich.edu>
2020-02-09 15:57:59 -0500

    data_simulation modification

--- data_simulation.py
+++ data_simulation.py
@@ -25,24 +25,59 @@ sigma = np.eye(num_predictors) # covariance matrix
 mu = np.zeros(num_predictors) # ith row for mu
 # parameters for lasso model
 a = 0.1 # for regularization
+# instances of data
+N = 20
 
-# may play around different generation process
-# (1) generate Y dim = n*1
-y = np.random.choice(a=[-1, 1], size=(num_obs,1), replace=True, p=[1-prob, prob])
-# (2) generate Xi
-# generate error terms dim = n*p 
-U = np.random.multivariate_normal(mean=mu, cov=sigma, size=num_obs)
-# multiply yi's and beta and add with U to get X, dimX = n*p
-X = np.dot(y, beta) + U
+def generate_data():
+    # may play around different generation process
+    # (1) generate Y dim = n*1
+    y = np.array(np.random.choice(a=[-1, 1], size=(num_obs,1), replace=True, p=[1-prob, prob]))
+    # (2) generate Xi
+    # generate error terms dim = n*p 
+    U = np.random.multivariate_normal(mean=mu, cov=sigma, size=num_obs)
+    # multiply yi's and beta and add with U to get X, dimX = n*p
+    X = np.dot(y, beta) + U
+    return X, y
 
-# (3) Apply to classifiers and do cross-validation to assess the average accuracy
-# 1: Lasso: (1 / (2 * n_samples)) * ||y - Xw||^2_2 + alpha * ||w||_1
-clf1 = linear_model.Lasso(alpha=a)
+def split_data(X, y)
+    X_train = []
+    y_train = []
+    X_test = []
+    y_test = []
+    # take the first 80% data as the training data
+    skf = StratifiedKFold(n_splits=5)
+    # only get the first set of indices
+    for train_idx, test_idx in skf.split(X, y):
+        X_train, X_test = X[train_idx], X[test_idx]
+        y_train, y_test = y[train_idx], y[test_idx]
+        break
+    return X_train, y_train, X_test, y_test
+
+def train_lasso_clf(alpha, X_train, y_train, X_test, y_test):
+    # 1: Lasso: (1 / (2 * n_samples)) * ||y - Xw||^2_2 + alpha * ||w||_1
+    clf = linear_model.Lasso(alpha=a)
+    clf.fit(X_train, y_train)
+    # calculate training accuracy and test accuracy
+    y_pred_test = calssifier.predict(X_test)
+    y_pred_train = calssifier.predict(X_train)
+    y_pred_test[y_pred_test > 0] = 1
+    y_pred_test[y_pred_test <= 0] = -1
+    y_pred_train[y_pred_train > 0] = 1
+    y_pred_train[y_pred_train <= 0] = -1
+    acc_test.append(metrics.accuracy_score(y_test, y_pred_test))
+    acc_train.append(metrics.accuracy_score(y_train, y_pred_train))
+    return acc_train, acc_test
+
+
+X, y = generate_data()
+train_lasso_clf(a, X, y)
+
+
 da = mlpy.DLDA(delta=0)
 def cross_validation(calssifier, X, y, k, mode="Lasso"):
     acc_test = []
     acc_train = []
-    skf = StratifiedKFold(n_splits=5)
+    
     for train_idx, test_idx in skf.split(X, y):
         X_train, X_test = X[train_idx], X[test_idx]
         y_train, y_test = y[train_idx], y[test_idx]
@@ -52,16 +87,19 @@ def cross_validation(calssifier, X, y, k, mode="Lasso"):
             calssifier.fit(X_train, y_train)
             y_pred_test = calssifier.predict(X_test)
             y_pred_train = calssifier.predict(X_train)
+            y_pred_test[y_pred_test > 0] = 1
+            y_pred_test[y_pred_test <= 0] = -1
+            y_pred_train[y_pred_train > 0] = 1
+            y_pred_train[y_pred_train <= 0] = -1
+            acc_test.append(metrics.accuracy_score(y_test, y_pred_test))
+            acc_train.append(metrics.accuracy_score(y_train, y_pred_train))
         elif mode == "DLDA":
+            y_train[y_train == -1] = 0
+            y_train = np.transpose(y_train)[0]
             calssifier.learn(X_train, np.transpose(y_train)[0])
             y_pred_test = calssifier.pred(X_test)
             y_pred_train = calssifier.pred(X_train)
-        y_pred_test[y_pred_test > 0] = 1
-        y_pred_test[y_pred_test <= 0] = -1
-        y_pred_train[y_pred_train > 0] = 1
-        y_pred_train[y_pred_train <= 0] = -1
-        acc_test.append(metrics.accuracy_score(y_test, y_pred_test))
-        acc_train.append(metrics.accuracy_score(y_train, y_pred_train))
+            print(y_pred_test) 
     return np.array(acc_test).mean(), np.array(acc_train).mean()
 
 acc_test, acc_train = cross_validation(clf1, X, y, 5, "Lasso")
